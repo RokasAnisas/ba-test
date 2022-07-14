8aca6a3feefad91ccc1bfcf606d213a25fbf457d
Rokas Anisas <rokas.anisas@gmail.com>
2022-07-14 22:32:23 +0300

    update dropdown menu

--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useId, useState } from 'react';
 import classNames from 'classnames/bind';
 import { MdOutlineSettings } from 'react-icons/md';
 
@@ -9,12 +9,27 @@ const cx = classNames.bind(styles);
 
 export const DropdownMenu: FC<DropdownMenuProps> = () => {
   const className = 'dropdown-menu';
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuId = useId();
 
   return (
     <div className={cx(className)}>
-      <button className={cx(`${className}__launcher`)}>
+      <button
+        className={cx(`${className}__launcher`, { 'is-open': menuOpen })}
+        aria-haspopup="true"
+        aria-controls={menuId}
+        onClick={() => setMenuOpen(state => !state)}
+      >
         <MdOutlineSettings />
       </button>
+      <ul
+        className={cx(`${className}__menu`, { 'is-open': menuOpen })}
+        id={menuId}
+        role="menu"
+      >
+        <li className={cx(`${className}__menu-item`)}>Menu item 1</li>
+        <li className={cx(`${className}__menu-item`)}>Menu item 2</li>
+      </ul>
     </div>
   );
 };
