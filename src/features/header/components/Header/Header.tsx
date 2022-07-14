bf77ce15ab53b498512b7d79dc261ff9d2a056b3
Rokas Anisas <rokas.anisas@gmail.com>
2022-07-14 23:41:12 +0300

    option toggle

--- src/features/header/components/Header/Header.tsx
+++ src/features/header/components/Header/Header.tsx
@@ -6,6 +6,12 @@ import { InfoText } from '@/components/InfoText';
 import { Button } from '@/components/Button';
 import { DropdownMenu } from '@/components/DropdownMenu';
 import { en } from '@/translations/en';
+import { OptionToggle } from '@/components/OptionToggle';
+import { useAppDispatch, useAppSelector } from '@/middleware/redux/redux.hooks';
+import {
+  selectTheme,
+  setTheme,
+} from '@/features/themeSwitcher/themeSwitcher.slice';
 
 import { HeaderProps } from './Header.props';
 
@@ -14,6 +20,8 @@ const cx = classNames.bind(styles);
 
 export const Header: FC<HeaderProps> = () => {
   const className = 'header';
+  const dispatch = useAppDispatch();
+  const currentTheme = useAppSelector(selectTheme);
 
   return (
     <header className={cx(className)}>
@@ -23,7 +31,34 @@ export const Header: FC<HeaderProps> = () => {
       <div className={cx(`${className}__container`, '-right')}>
         <InfoText description={en.pressSpaceToShuffleOr} />
         <Button>{en.clickHere}</Button>
-        <DropdownMenu items={[{ label: 'Grid size' }, { label: 'Theme' }]} />
+        <DropdownMenu
+          items={[
+            {
+              label: 'Theme',
+              action: (
+                <OptionToggle
+                  options={[
+                    {
+                      label: 'dark',
+                      onClick() {
+                        dispatch(setTheme('dark'));
+                      },
+                      active: currentTheme === 'dark',
+                    },
+                    {
+                      label: 'light',
+                      onClick() {
+                        dispatch(setTheme('light'));
+                      },
+                      active: currentTheme === 'light',
+                    },
+                  ]}
+                />
+              ),
+            },
+            { label: 'Grid size' },
+          ]}
+        />
       </div>
     </header>
   );
--- src/features/themeSwitcher/themeSwitcher.slice.ts
+++ src/features/themeSwitcher/themeSwitcher.slice.ts
@@ -4,12 +4,10 @@ import type { RootState } from '@/middleware/redux/redux.store';
 
 import { Theme } from './themeSwitcher.types';
 
-// Define a type for the slice state
 interface ThemeSwitcherState {
   theme: Theme;
 }
 
-// Define the initial state using that type
 const initialState: ThemeSwitcherState = {
   theme: 'dark',
 };
@@ -18,29 +16,14 @@ export const themeSwitcherSlice = createSlice({
   name: 'ThemeSwitcher',
   initialState,
   reducers: {
-    // Use the PayloadAction type to declare the contents of `action.payload`
     setTheme: (state, action: PayloadAction<Theme>) => {
       state.theme = action.payload;
-    },
-    toggleTheme: state => {
-      switch (state.theme) {
-        case 'light':
-          state.theme = 'dark';
-          break;
-        case 'dark':
-          state.theme = 'light';
-          break;
-        default:
-          state.theme = 'light';
-          break;
-      }
     },
   },
 });
 
-export const { setTheme, toggleTheme } = themeSwitcherSlice.actions;
+export const { setTheme } = themeSwitcherSlice.actions;
 
-// Other code such as selectors can use the imported `RootState` type
 export const selectTheme = (state: RootState) => state.themeSwitcher.theme;
 
 export default themeSwitcherSlice.reducer;
