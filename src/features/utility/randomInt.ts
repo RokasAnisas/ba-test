98d10c1f875e5194490f78ca15f0bd30155d3b5b
Rokas Anisas <rokas.anisas@gmail.com>
2022-07-15 13:54:14 +0300

    Update randomInt.ts

--- src/features/utility/randomInt.ts
+++ src/features/utility/randomInt.ts
@@ -1,9 +1,3 @@
-import { useCallback } from 'react';
-
 export const randomInt = (min: number, max: number) => {
   return Math.floor(min + Math.random() * (max - min));
-};
-
-export const useRandomInt = (min: number, max: number) => {
-  return useCallback(() => randomInt(min, max), [min, max]);
 };
